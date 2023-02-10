3931ad8e0e613850a755a82f740c1f89df42fb9e
Documenter.jl <documenter@juliadocs.github.io>
2023-02-10 09:59:27 +0000

    build based on 87803fb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MojiMoji","category":"page"},{"location":"#MojiMoji","page":"Home","title":"MojiMoji","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MojiMoji.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MojiMoji]","category":"page"}]
+}
